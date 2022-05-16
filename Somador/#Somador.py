a174147d638ed3fb5c7a8ac677c8390554f02661
LeandroLDA <45002731+LeandroLDA@users.noreply.github.com>
2022-05-15 22:42:39 -0300

    Adicionado Somador.PY

---
+++ Somador/#Somador.py
@@ -0,0 +1,12 @@
+#somador
+x = int(nput("Entre com o primeniro valor:\n"))
+y = int(inut("Entre com o segundo valor:\n"))
+
+if x > y:
+    r=x+y
+    print(r)
+else:
+    print("Soma não execuada!!!\n")  
+
+
+  
